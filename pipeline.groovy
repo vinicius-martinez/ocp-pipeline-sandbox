94ea6706f763bc2808f85db12e72afd98f23758d
vinicius-martinez <vinicius.b.martinez@gmail.com>
2018-05-11 18:04:48 -0300

    fixing issues -- script tag missing

--- pipeline.groovy
+++ pipeline.groovy
@@ -18,14 +18,14 @@ pipeline {
       }
     }
 
-    stage ('Input Test') {
+    stage ('ChoiceParameterDefinition Test') {
       steps {
         script {
-          def userInput = input(id: 'userInput', message: 'Merge to?',
+          def choiceInput = input(id: 'userInput', message: 'Merge to?',
           parameters: [[$class: 'ChoiceParameterDefinition', defaultValue: 'strDef',
              description:'describing choices', name:'nameChoice', choices: "QA\nUAT\nProduction\nDevelop\nMaster"]
           ])
-         println(userInput);
+         println "Value from choiceInput ${choiceInput}";
        }
      }
    }
